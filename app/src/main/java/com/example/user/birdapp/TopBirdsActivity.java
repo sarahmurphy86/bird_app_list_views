757787b849175565ff3072540bd2a339e27cc533
sarahmurphy86 <sarah.graham86@gmail.com>
2018-05-30 15:32:56 +0100

    bird item created

---
+++ app/src/main/java/com/example/user/birdapp/TopBirdsActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.birdapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopBirdsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.birds_list);
+    }
+}
